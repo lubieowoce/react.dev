7e714cf06438f764e100802313382eaf4904a23f
lubieowoce <lolzatu2@gmail.com>
2024-03-11 03:17:56 +0100

    a program for two iframes

--- src/components/MDX/Sandpack/SandpackRoot.tsx
+++ src/components/MDX/Sandpack/SandpackRoot.tsx
@@ -6,6 +6,8 @@ import {Children} from 'react';
 import * as React from 'react';
 import {
   SandpackProvider,
+  SandpackState,
+  UseSandpack,
   useSandpack,
 } from '@codesandbox/sandpack-react/unstyled';
 import {SandpackLogLevel} from '@codesandbox/sandpack-client';
@@ -80,30 +82,86 @@ function SandpackRoot(props: SandpackProps) {
     hidden: !files['/src/styles.css']?.visible,
   };
 
+  const sandboxIds = useClientServerSandboxIds();
+  const clientSandpack = React.useRef<UseSandpack | null>(null);
+  const serverSandpack = React.useRef<UseSandpack | null>(null);
+  const sandpacks = React.useMemo(
+    () => ({
+      server: serverSandpack,
+      client: clientSandpack,
+    }),
+    []
+  );
+  const [messageChannel] = React.useState(() => {
+    const channel = new MessageChannel();
+    return {
+      server: channel.port1,
+      client: channel.port2,
+    };
+  });
+
+  const sendRequestGeneric = usePostMessageRequest();
+  const sendIframeRequest = useIframeRequest();
+  React.useEffect(() => {
+    const onMessage = (event: MessageEvent<unknown>) => {
+      const {data} = event;
+      if (!data || typeof data !== 'object') {
+        return;
+      }
+      if ('__rsc_init' in data) {
+        const body = data.__rsc_init as {sandboxId: string};
+        const target = sandboxIds.reverse[body.sandboxId];
+        console.log('SandpackRoot :: sandbox bootstrapped:', target);
+
+        const {iframe} = getClientFromSandpackState(
+          sandpacks[target].current!.sandpack
+        );
+        const channelPort = messageChannel[target];
+        console.log('sending port...', target);
+        void sendIframeRequest('RSC_CHANNEL_PORT', [channelPort], iframe);
+      }
+    };
+    window.addEventListener('message', onMessage, false);
+    return () => window.removeEventListener('message', onMessage, false);
+  }, [
+    sandboxIds.reverse,
+    messageChannel,
+    sandpacks,
+    sendRequestGeneric,
+    sendIframeRequest,
+  ]);
+
   return (
     <div className="sandpack sandpack--playground w-full my-8" dir="ltr">
-      {/* TODO: unflag when stuff works! */}
-      {!rsc && (
-        <SandpackProvider
-          files={{...template, ...files}}
-          theme={CustomTheme}
-          customSetup={{
-            environment: 'react' as any,
-          }}
-          options={{
-            bundlerTimeOut: Infinity,
-            autorun,
-            initMode: 'user-visible',
-            initModeObserverOptions: {rootMargin: '1400px 0px'},
-            bundlerURL: 'http://localhost:1234/', // github.com:lubieowoce/sandpack-bundler.git 3cd6682
-            // bundlerURL: 'https://786946de.sandpack-bundler-4bw.pages.dev',
-            // logLevel: SandpackLogLevel.None,
-            logLevel: SandpackLogLevel.Debug,
-          }}>
-          <CustomPreset providedFiles={Object.keys(files)} />
-          {/* {rsc && typeof window !== undefined && <RSCConnection />} */}
-        </SandpackProvider>
-      )}
+      <SandpackProvider
+        files={{
+          ...template,
+          ...(rsc
+            ? {
+                ...Object.fromEntries(Object.entries(files)),
+                ...rscClientLibFiles,
+                ...createSandboxIdFile(sandboxIds.client),
+              }
+            : files),
+        }}
+        theme={CustomTheme}
+        customSetup={{
+          environment: 'react' as any,
+        }}
+        options={{
+          bundlerTimeOut: Infinity,
+          autorun,
+          initMode: 'user-visible',
+          initModeObserverOptions: {rootMargin: '1400px 0px'},
+          bundlerURL: 'http://localhost:1234/', // github.com:lubieowoce/sandpack-bundler.git 3cd6682
+          // bundlerURL: 'https://786946de.sandpack-bundler-4bw.pages.dev',
+          logLevel: SandpackLogLevel.None,
+          // logLevel: SandpackLogLevel.Debug,
+        }}>
+        <CustomPreset providedFiles={Object.keys(files)} />
+        <ReadSandpack sandpackRef={clientSandpack} />
+        {/* {rsc && typeof window !== undefined && <RSCConnection />} */}
+      </SandpackProvider>
       {rsc && (
         <SandpackProvider
           files={{
@@ -115,6 +173,7 @@ function SandpackRoot(props: SandpackProps) {
                 {code, hidden: true},
               ])
             ),
+            ...createSandboxIdFile(sandboxIds.server),
           }}
           theme={CustomTheme}
           customSetup={{
@@ -127,24 +186,67 @@ function SandpackRoot(props: SandpackProps) {
             initModeObserverOptions: {rootMargin: '1400px 0px'},
             bundlerURL: 'http://localhost:1234/', // github.com:lubieowoce/sandpack-bundler.git 3cd6682
             // bundlerURL: 'https://786946de.sandpack-bundler-4bw.pages.dev',
-            // logLevel: SandpackLogLevel.None,
-            logLevel: SandpackLogLevel.Debug,
+            logLevel: SandpackLogLevel.None,
+            // logLevel: SandpackLogLevel.Debug,
           }}>
+          <ReadSandpack sandpackRef={serverSandpack} />
           <CustomPreset providedFiles={Object.keys(files)} />
-          {rsc && typeof window !== undefined && <RSCConnection />}
+          {/* {rsc && typeof window !== undefined && <RSCConnection />} */}
         </SandpackProvider>
       )}
     </div>
   );
 }
 
+function useClientServerSandboxIds() {
+  const idBase = 'sandbox-' + React.useId().replace(':', '-');
+  return React.useMemo(() => {
+    const client = idBase + '-' + 'client';
+    const server = idBase + '-' + 'server';
+    const reverse = {
+      [client]: 'client' as const,
+      [server]: 'server' as const,
+    };
+    return {client, server, reverse};
+  }, [idBase]);
+}
+
+const ReadSandpack = React.memo(function ReadSandpack({
+  sandpackRef,
+}: {
+  sandpackRef: React.MutableRefObject<UseSandpack | null>;
+}) {
+  const value = useSandpack();
+  React.useEffect(() => {
+    sandpackRef.current = value;
+  });
+  React.useEffect(() => {
+    sandpackRef.current = null;
+  }, [sandpackRef]);
+  return null;
+});
+
+function createSandboxIdFile(id: string) {
+  return {
+    'src/__rsc__/sandbox-id.js': {
+      hidden: true,
+      code: `export default ${JSON.stringify(id)}`,
+    },
+  };
+}
+
+const PLACEHOLDER_ID_FILE = `throw new Error("'src/__rsc__/sandbox-id.js' was not replaced"); export {}`;
+
 const rscServerLibFiles = {
+  'src/__rsc__/sandbox-id.js': PLACEHOLDER_ID_FILE,
   'src/index.js': `
 import './__rsc__/webpack.js';
+import { initMessaging, createRequestListener } from './__rsc__/init-messaging.js';
+
 import App from './App.js'
 import * as RSDWServer from 'react-server-dom-webpack/server';
 
-async function handleRequest() {
+async function handleRenderRequest() {
   const stream = await RSDWServer.renderToReadableStream(
     <App />,
     moduleMap,
@@ -159,62 +261,199 @@ const moduleMap = new Proxy({}, {
     const prefix = 'file://';
     const moduleName = moduleUrl.startsWith(prefix) ? moduleUrl.slice(prefix.length) : moduleUrl
     // console.log('moduleMap', { moduleName, exportName });
-    return { id: moduleName, chunks: [moduleName], name: exportName }
+    return { id: moduleName, chunks: [moduleName], name: exportName, async: true }
   }
 });
 
-const listener = async (event) => {
-  // console.log('RSC frame got message', event)
-  const { data } = event;
-  if (!data.__rsc_request) {
-    return
+let cleanupPrevious;
+initMessaging((port) => {
+  if (cleanupPrevious) {
+    cleanupPrevious();
   }
-
-  const { requestId } = data.__rsc_request;
-
-  try {
-    const stream = await handleRequest();
-    // console.log('RSC frame', 'responding...', { requestId })
-    window.parent.postMessage(
-      { __rsc_response: { requestId, data: stream } },
-      '*',
-      [stream]
-    );
-  } catch (error) {
-    window.parent.postMessage(
-      { __rsc_response: { requestId, error: error.message ?? \`$\{error}\` } },
-      '*',
-    );
-  }
-}
-
-// nastily handle hot reload
-if (window.__RSC_LISTENER && window.__RSC_LISTENER !== listener) {
-  window.removeEventListener(
-    "message",
-    window.__RSC_LISTENER,
-    false,
+  console.log('rsc-server :: attaching request listener to port', port);
+  const requestListener = createRequestListener(
+    (data) => {
+      if (data) { return }
+      return handleRenderRequest();
+    },
+    { 
+      sendReply: (...args) => port.postMessage(...args),
+      name: 'rsc-server :: RSC_CHANNEL_PORT',
+      debug: true,
+    }
   );
-  window.__RSC_LISTENER = listener;
+  port.addEventListener('message', requestListener);
+  port.start();
+  console.log('rsc-server :: listening');
+  cleanupPrevious = () => {
+    port.removeEventListener('message', requestListener);
+  };
+});
+
+
+
+`,
+  '/src/__rsc__/init-messaging.js': `
+import sandboxId from './sandbox-id.js';
+
+const replyOnWindow = (data, transfer) => {
+  window.parent.postMessage(data, '*', transfer);
 }
 
-window.addEventListener(
-  "message",
-  listener,
-  false,
-);
+export function createRequestListener(
+  handler,
+  { sendReply = replyOnWindow, name = 'RSC frame', debug = false } = {}
+) {
+  return async (event) => {
+    debug && console.log(name + ' got message', event);
+    const { data } = event;
+    if (!data || typeof data !== 'object') {
+      return;
+    }
+    if (!data.__rsc_request) {
+      return;
+    }
+    debug && console.log(name + ' got request', event.data.__rsc_request);
+
+    const { requestId, data: requestData } = data.__rsc_request;
+
+    try {
+      const response = await handler(requestData, event);
+      debug && console.log(name, 'responding...', { requestId });
+      sendReply(
+        { __rsc_response: { requestId, data: response } },
+        (response && typeof response === 'object') ? [response] : undefined,
+      );
+    } catch (error) {
+      sendReply(
+        { __rsc_response: { requestId, error: error.message ?? \`$\{error}\` } },
+      );
+    }
+  }
+}
+
+export function createRequestClient(idBase = "") {
+  const idRef = { current: 0 };
+  return (
+    /** @type {any} */ data,
+    /** @type {any[] | undefined} */ transfer,
+    /** @type {{ postMessage; responseTarget: EventTarget; maxDuration?: number; }} */ options
+  ) =>
+    new Promise((resolve, reject) => {
+      const requestId = idBase + idRef.current++;
+      const { postMessage, responseTarget, maxDuration = 10_000 } = options;
+
+      const timeout = setTimeout(() => {
+        reject(
+          new Error('Did not receive response within ' + (maxDuration / 1000) +'s')
+        );
+        cleanup();
+      }, 10_000);
+
+      const responseHandler = (/** @type {MessageEvent<unknown>} */ event) => {
+        const { data } = event;
+        if (
+          !data ||
+          typeof data !== "object" ||
+          !("__rsc_response" in data) ||
+          !data.__rsc_response
+        ) {
+          return;
+        }
+        /** @type {{requestId: string} & ({data: unknown} | {error: string})} */
+        const response = data.__rsc_response;
+        if (response.requestId !== requestId) {
+          return;
+        }
+        if ("error" in response) {
+          reject(new Error(response.error));
+        } else if (!("data" in response)) {
+          reject(new Error("No data or error in response"));
+        } else {
+          resolve(response.data);
+        }
+
+        cleanup();
+        clearTimeout(timeout);
+      };
+
+      responseTarget.addEventListener(
+        "message",
+        /** @type {EventListener} */ (responseHandler),
+        false
+      );
+      const cleanup = () =>
+        responseTarget.removeEventListener(
+          "message",
+          /** @type {EventListener} */ (responseHandler),
+          false
+        );
+
+      postMessage({ __rsc_request: { requestId: requestId, data } }, transfer);
+    });
+}
+
+
+export function initMessaging(onPortReceived) {
+  addMessageListenerWithCleanup(
+    createRequestListener((data, event) => {
+      if (data === 'RSC_CHANNEL_PORT') {
+        const port = event.ports[0];
+        onPortReceived(port);
+      }
+    }),
+    '__rsc_channel_port'
+  );
+
+  window.parent.postMessage(
+    { __rsc_init: { sandboxId } },
+    '*',
+  );
+}
+
+
+function addMessageListenerWithCleanup(listener, name) {
+  if (window[name] && window[name] !== listener) {
+    window.removeEventListener(
+      "message",
+      window[name],
+      false,
+    );
+    window[name] = listener;
+  }
+  window.addEventListener("message", listener, false);
+};
+
 `,
   '/src/__rsc__/webpack.js': `
-  // console.log("installing __webpack_require__...");
 
 const moduleCache = new Map();
+
+function trackThenableState(promise) {
+  if (typeof promise.status === 'string') {
+    return promise
+  }
+  promise.status = "pending";
+  promise.then(
+    (value) => {
+      promise.status = "fulfilled";
+      promise.value = value;
+    },
+    (error) => {
+      promise.status = "rejected";
+      promise.reason = error;
+    }
+  );
+  return promise;
+}
 
 const getOrImport = (/** @type {string} */ id) => {
   // in sandpack's case, modules and chunks are one and the same.
   if (!moduleCache.has(id)) {
-    const promise = import(id);
+    const promise = trackThenableState(import(id));
     moduleCache.set(id, promise);
   }
+
   return moduleCache.get(id);
 }
 
@@ -234,8 +473,15 @@ if (typeof globalThis["__webpack_require__"] !== "function") {
   /** @type {Map<string, Promise<Record<string, unknown>>>} */
   globalThis["__webpack_require__"] = (/** @type {string} */ id) => {
     // console.log('__webpack_require__', id);
-
-    return getOrImport(id);
+    
+    const promise = getOrImport(id);
+    // this is important because we can't easily get $$async set on our references,
+    // and our imports are always async.
+    // luckily, we always preload the modules, so this should be fulfilled at this point.
+    if (promise.status === "fulfilled") {
+      return promise.value;
+    }
+    return promise;
   };
 }
 
@@ -243,15 +489,98 @@ export {};
 `,
 };
 
-function RSCConnection() {
-  const {sandpack, listen} = useSandpack();
-  // console.log('RSCConnection ::', sandpack.clients);
-  const clients = Object.values(sandpack.clients);
+const rscClientLibFiles = {
+  ...rscServerLibFiles,
+  'src/index.js': `
+import './__rsc__/webpack.js';
+import { initMessaging, createRequestClient } from './__rsc__/init-messaging.js';
+import { use, useState, startTransition } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import * as RSDWClient from 'react-server-dom-webpack/client';
+
+
+function promiseWithResolvers() {
+  let resolve, reject;
+  const promise = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  });
+  return { promise, resolve, reject }
+}
+
+let didResolve = false;
+const initialPromiseCtrl = promiseWithResolvers();
+const setElementPromise = (promise) => {
+  if (!didResolve) {
+    initialPromiseCtrl.resolve(promise);
+    didResolve = true;
+  } else {
+    setCurrentPromise(promise)
+  }
+}
+
+let setCurrentPromise;
+function Root({ initialPromise }) {
+  const [promise, _setCurrentPromise] = useState(initialPromise);
+  setCurrentPromise = _setCurrentPromise;
+  return use(promise);
+}
+
+const root = createRoot(document.getElementById('root'));
+startTransition(() => {
+  root.render(<Root initialPromise={initialPromiseCtrl.promise} />)
+});
+
+
+initMessaging((port) => {
+  console.log('rsc-client :: got port');
+  const sendRequest = createRequestClient();
+
+  (async () => {
+    try {
+      console.log('rsc-client :: requesting...');
+      const responseStream = await sendRequest(undefined, undefined, {
+        postMessage: (...args) => { port.start(); port.postMessage(...args) },
+        responseTarget: port,
+      });
+      console.log('rsc-client :: got response', responseStream);
+      if (!(responseStream instanceof ReadableStream)) {
+        throw new Error('Received response is not a ReadableStream');
+      }
+
+      const [s1, s2] = responseStream.tee();
+      const elementPromise = RSDWClient.createFromReadableStream(s1);
+      console.log('rsc-client :: element promise', elementPromise)
+      setElementPromise(elementPromise);
+
+      const decoder = new TextDecoder();
+      for await (const chunk of s2) {
+        console.log(decoder.decode(chunk))
+      }
+
+    } catch (error) {
+      setElementPromise(Promise.reject(error));
+    }
+  })();
+});
+
+`,
+};
+
+const getClientFromSandpackState = (sandpackState: SandpackState) => {
+  const clients = Object.values(sandpackState.clients);
   if (clients.length > 1) {
     throw new Error('Multiple sandpack clients');
   }
+  return clients[0] ?? null;
+};
+
+function RSCConnection() {
+  const {sandpack, listen} = useSandpack();
+  // console.log('RSCConnection ::', sandpack.clients);
+  const client = getClientFromSandpackState(sandpack);
   const sendRequest = useIframeRequest();
-  const client = clients[0] ?? null;
   const onCompilationDone = React.useCallback(
     (success: boolean) => {
       if (!success) {
@@ -269,7 +598,7 @@ function RSCConnection() {
         client
       );
       console.log('RSCConnection :: Sending request to iframe');
-      sendRequest(client.iframe).then(async (_stream) => {
+      sendRequest(undefined, [], client.iframe).then(async (_stream) => {
         const stream = _stream as ReadableStream<Uint8Array>;
         const decoder = new TextDecoder();
 
@@ -300,18 +629,40 @@ function RSCConnection() {
 }
 
 function useIframeRequest() {
+  const sendRequestImpl = usePostMessageRequest();
+  return React.useCallback(
+    (data: any, transfer: any[] | undefined, iframe: HTMLIFrameElement) => {
+      return sendRequestImpl(data, transfer, {
+        postMessage: (data, transfer = undefined) =>
+          iframe.contentWindow!.postMessage(data, '*', transfer),
+        responseTarget: window,
+      });
+    },
+    [sendRequestImpl]
+  );
+}
+
+function usePostMessageRequest() {
   const idBase = React.useId();
   const idRef = React.useRef(0);
 
-  const sendRequest = React.useCallback(
-    (iframe: HTMLIFrameElement) =>
+  return React.useCallback(
+    (
+      data: any,
+      transfer: any[] | undefined,
+      options: {
+        postMessage: (data: any, transfer?: any[] | undefined) => void;
+        responseTarget: EventTarget;
+        maxDuration?: number;
+      }
+    ) =>
       new Promise((resolve, reject) => {
         const requestId = idBase + idRef.current++;
-        const MAX_DURATION = 10_000;
+        const {postMessage, responseTarget, maxDuration = 10_000} = options;
 
         const timeout = setTimeout(() => {
           reject(
-            new Error(`Did not receive response within ${MAX_DURATION / 1000}s`)
+            new Error(`Did not receive response within ${maxDuration / 1000}s`)
           );
           cleanup();
         }, 10_000);
@@ -345,19 +696,22 @@ function useIframeRequest() {
           clearTimeout(timeout);
         };
 
-        window.addEventListener('message', responseHandler, false);
-        const cleanup = () =>
-          window.removeEventListener('message', responseHandler, false);
-
-        iframe.contentWindow!.postMessage(
-          {__rsc_request: {requestId: requestId}},
-          '*'
+        responseTarget.addEventListener(
+          'message',
+          responseHandler as EventListener,
+          false
         );
+        const cleanup = () =>
+          responseTarget.removeEventListener(
+            'message',
+            responseHandler as EventListener,
+            false
+          );
+
+        postMessage({__rsc_request: {requestId: requestId, data}}, transfer);
       }),
     [idBase]
   );
-
-  return sendRequest;
 }
 
 export default SandpackRoot;
